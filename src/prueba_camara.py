a38fee739eb65013fd8e2b7526d688e979a734e3
jOSE HURTADO <j.hurtado1@utp.edu.co>
2026-03-10 20:05:22 -0500

    control de motores y encoder

---
+++ src/prueba_camara.py
@@ -0,0 +1,16 @@
+from picamera2 import Picamera2
+import time
+
+try:
+    picam2 = Picamera2()
+    print("Cámaras detectadas:", picam2.global_camera_info())
+
+    picam2.start()
+    time.sleep(2)
+    picam2.capture_file("foto_prueba.jpg")
+    picam2.stop()
+
+    print("Foto tomada correctamente")
+
+except Exception as e:
+    print("Error:", e)
